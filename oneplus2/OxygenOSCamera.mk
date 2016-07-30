aa34d4f8ed08f08eb89bc32416e9e1221643721c
xdevs23 <xdevs23@outlook.com>
2016-07-30 12:22:40 +0200

    OxygenOSCamera: don't ship OnePlusCameraService
    Not neccessary, works perfectly fine without this and this might be draining battery.
    
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- oneplus2/OxygenOSCamera.mk
+++ oneplus2/OxygenOSCamera.mk
@@ -95,19 +95,6 @@ LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 
 include $(BUILD_PREBUILT)
 
-# OnePlusCameraService
-include $(CLEAR_VARS)
-
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := OnePlusCameraService
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_SRC_FILES := proprietary/priv-app/OnePlusCameraService/OnePlusCameraService.apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-
-include $(BUILD_PREBUILT)
-
 ### VENDOR
 
 # lib-imscamera.so
