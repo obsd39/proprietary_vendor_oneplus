fffe8c366597114a08c6f8e27a6b099fb897e7e4
Kshitij Gupta <kshitijgm@gmail.com>
2018-01-13 22:59:59 +0530

    bacon: Remove thermal-engine copy rule
    - The file exists no more, this just throws an error
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- bacon-vendor.mk
+++ bacon-vendor.mk
@@ -171,7 +171,6 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/bacon/proprietary/lib/libloc_api_v02.so:system/lib/libloc_api_v02.so \
     vendor/oneplus/bacon/proprietary/lib/libloc_ds_api.so:system/lib/libloc_ds_api.so \
     vendor/oneplus/bacon/proprietary/lib/libwcnss_qmi.so:system/lib/libwcnss_qmi.so \
-    vendor/oneplus/bacon/proprietary/vendor/bin/thermal-engine:system/vendor/bin/thermal-engine \
     vendor/oneplus/bacon/proprietary/vendor/lib/egl/eglsubAndroid.so:system/vendor/lib/egl/eglsubAndroid.so \
     vendor/oneplus/bacon/proprietary/vendor/lib/egl/libEGL_adreno.so:system/vendor/lib/egl/libEGL_adreno.so \
     vendor/oneplus/bacon/proprietary/vendor/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/egl/libGLESv1_CM_adreno.so \
